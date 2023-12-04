a244809eafb73492bfd3f9ded164768603c680a3
Жавлон Турдиматов <j.turdimatov@dkp.local>
2023-12-04 12:01:29 +0500

    like percentage fixed

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,7 +1,6 @@
 import ApiError from '../services/appError.js';
 import logger from '../config/logger.js';
 
-
 export default function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message, errors: err.errors });
--- services/tokenservice.js
+++ services/tokenservice.js
@@ -6,7 +6,7 @@ config();
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '15s',
+      expiresIn: '1d',
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
       expiresIn: '30d',
